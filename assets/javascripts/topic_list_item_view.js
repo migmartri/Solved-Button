7e4df42ccadb3ecbd88ae3c7a2eb6ad98e2417ea
Miguel <miguel@bitnami.com>
2014-06-19 14:05:16 -0700

    Adding odd:even colors

--- assets/javascripts/topic_list_item_view.js
+++ assets/javascripts/topic_list_item_view.js
@@ -3,19 +3,13 @@ Discourse.TopicListItemView.reopen({
     var session = Discourse.Session.current();
     var $topic = this.$();
     var originalCol = $topic.css('backgroundColor').toString();
-    //
-    // TODO: improve
-    solved = this.get('content.is_solved') == true;
-    if (solved) {
-      var color = originalCol.toString().replace("rgba","").replace("rgb","").replace("(", "").replace(")", "").replace(" ","").split(",");
-      // workaround for no background
-      if ((color[0] == 0) && (color[1] == 0) && (color[2] == 0))
-      {
-          color = [255,255,255];
-      }
-      color[0] = Math.round(color[0] * 0.95);
-      color[2] = Math.round(color[2] * 0.95);
-      color = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
+
+    if (!!this.get('content.is_solved')) {
+      if ($topic.is(":nth-child(odd)"))
+        color = '#f0fff0';
+      else
+        color = '#eaf7ea';
+      // use backgroundColor to avoid highlight changing the color
       $topic
         .css('backgroundColor', color);
     }
